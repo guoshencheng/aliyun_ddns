92365bcf93124d38a352595bad9be7498f4c7540
guoshencheng <648772021@qq.com>
2020-11-22 10:15:11 +0800

    feat: 添加ipv6的配置

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const Core = require('@alicloud/pop-core');
+const publicIp = require('public-ip')
 console.log('start print env info ====')
 console.log({
   accessKeyId: process.env.ACCESS_KEY,
@@ -17,29 +18,45 @@ const requestOption = {
   method: 'POST'
 };
 
-const recordId = '20704900814693376'
-
-const params = (value) => ({
+const params = (RR, Type, recordId, value) => ({
   DomainName: process.env.DOMAIN,
-  RR: '@',
-  Type: 'A',
+  RR,
+  Type,
   recordId,
   value,
 })
 
 
-let _ip = ''
 const schedule = require('node-schedule');
+
 const action = async () => {
+  await setV6()
+  await setV4()
+}
+let _ip = ''
+const setV4 = async () => {
   try {
     const res = await axios.get('http://www.pubyun.com/dyndns/getip')
     const ip = res.data.trim()
     if (/^(\d+).(\d+).(\d+).(\d+)$/.test(ip) && _ip !== ip) {
       _ip = ip
       console.log(`update ddns with ip ${_ip}`)
-      const updateRes = await client.request('UpdateDomainRecord', params(_ip), requestOption)
+      const updateRes = await client.request('UpdateDomainRecord', params('@', 'A', '20786455750181888', _ip), requestOption)
       console.log(JSON.stringify(updateRes))
     }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+let _ipv6 = ''
+const setV6 = async () => {
+  try {
+    const ipv6 = await publicIp.v6()
+    _ipv6 = ipv6
+    console.log(`update ddns with ipv6 ${_ipv6}`)
+    const updateRes = await client.request('UpdateDomainRecord', params('v6', 'AAAA', '20704900814693376', _ipv6), requestOption)
+    console.log(JSON.stringify(updateRes))
   } catch (error) {
     console.log(error)
   }
